5de85ef3725ecb3b1f2fe7964260c28f6526e15f
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2021-08-30 21:31:40 +0200

    Update client.h

--- headers/client.h
+++ headers/client.h
@@ -1,9 +1,13 @@
 #ifndef PROGETTOSOL_CLIENT_H
 #define PROGETTOSOL_CLIENT_H
 
-int sock_fd = 0;
-char *r_dirname = NULL;
-char *w_dirname = NULL;
+#include "./queue.h"
+
+extern int sockfd;
+extern char *r_dir;
+extern char *w_dir;
+extern char *sockname;
+extern struct timespec time_to_wait;
 
 void client_cleanup(argsQueue_t *queue, commandline_arg_t *curr_arg);
 
