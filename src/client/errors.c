a94532c8ef2230b862027308702abe3532c6ffe3
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2021-08-30 21:30:51 +0200

    Add files via upload

---
+++ src/client/errors.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include "../../headers/errors.h"
+
+void my_perror(const char *string) {
+
+	switch (local_errno) {
+
+		case ESOCK: {
+
+			fprintf(stderr, "%s ", string);
+			perror("socket()");
+
+		} break;
+
+		case ECONN: {
+
+			fprintf(stderr, "%s ", string);
+			perror("connect()");
+
+		} break;
+
+		case EREADN: {
+
+			fprintf(stderr, "%s ", string);
+			perror("readn()");
+
+		} break;
+
+		case EWRITEN: {
+
+			fprintf(stderr, "%s ", string);
+			perror("writen()");
+
+		} break;
+
+		case ESERVER: {
+
+			fprintf(stderr, "%s server error", string);
+
+		} break;
+
+		case ECALLOC: {
+
+			fprintf(stderr, "%s ", string);
+			perror("calloc()");
+
+		} break;
+
+		case ECLOSE: {
+
+			fprintf(stderr, "%s ", string);
+			perror("close()");
+
+		} break;
+
+		case EREALLOC: {
+
+			fprintf(stderr, "%s ", string);
+			perror("realloc()");
+
+		} break;
+
+		case EFOPEN: {
+
+			fprintf(stderr, "%s ", string);
+			perror("fopen()");
+
+		} break;
+
+		default: {
+
+			fprintf(stderr, "%s ", string);
+			perror(NULL);
+
+		} break;
+	}
+}
