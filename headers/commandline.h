480ef0d62642afbe4779d7a22cc6a2c766552dfa
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2021-09-09 22:51:40 +0200

    Create commandline.h

---
+++ headers/commandline.h
@@ -0,0 +1,11 @@
+#ifndef PROGETTOSOL_COMMANDLINE_H
+#define PROGETTOSOL_COMMANDLINE_H
+
+typedef struct cmdline_arg {
+
+	char option;
+	char **args;
+
+} commandline_arg_t;
+
+#endif //PROGETTOSOL_COMMANDLINE_H
