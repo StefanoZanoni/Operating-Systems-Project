30b3cf9beb7ef605226b2355b6b031d9832dd84a
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2021-09-10 11:23:48 +0200

    Delete headers directory

--- headers/commands_execution.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROGETTOSOL_COMMANDS_EXECUTION_H
-#define PROGETTOSOL_COMMANDS_EXECUTION_H
-
-#include "./queue.h"
-
-void execute_h();
-int execute_command(argnode_t *command, argsQueue_t *queue);
-
-#endif //PROGETTOSOL_COMMANDS_EXECUTION_H
--- headers/parsing.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROGETTOSOL_PARSING_H
-#define PROGETTOSOL_PARSING_H
-
-void commands_parsing(int argc, char **argv, argsQueue_t *queue);
-
-#endif //PROGETTOSOL_PARSING_H
