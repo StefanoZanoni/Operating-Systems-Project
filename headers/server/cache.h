cc2e0d99a4d872c97c5bb3c2af8a433753c69393
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2021-09-10 11:24:31 +0200

    Add files via upload

---
+++ headers/server/cache.h
@@ -0,0 +1,67 @@
+#ifndef PROGETTOSOL_CACHE_H
+#define PROGETTOSOL_CACHE_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#include "configuration.h" 
+
+typedef struct atomic_fidx {
+
+	pthread_mutex_t fidx_mutex;
+	unsigned long int index;
+
+} atomic_fidx_t;
+
+typedef struct my_file {
+
+	unsigned long int fidx;
+	unsigned long int file_size;
+	bool is_locked;
+	char *pathname;
+	void *content;
+
+} myfile_t;
+
+typedef struct mynode {
+
+	struct mynode *prev,
+				  *next;
+	myfile_t *file;
+
+} node_t;
+
+typedef struct myqueue {
+
+	pthread_mutex_t queue_mutex;
+	unsigned long int max_capacity;
+	unsigned long int curr_capacity;
+	unsigned long int max_files;
+	unsigned long int curr_files;
+	unsigned long int num_ejected;
+	node_t *head,
+		   *tail;
+
+} queue_t;
+
+typedef struct hashtable {
+
+	pthread_mutex_t table_mutex;
+	unsigned long int capacity;
+	node_t **files_references;
+
+} hashtable_t;
+
+int cache_initialization(server_configuration_t config, hashtable_t *table, queue_t *queue);
+int is_queue_empty(queue_t *queue);
+int capacity_reached(queue_t *queue);
+int files_reached(queue_t *queue);
+node_t* new_node(myfile_t *f);
+void dequeue(queue_t *queue);
+int enqueue(queue_t *queue, hashtable_t *table, myfile_t *file);
+void reference_file(queue_t *queue, hashtable_t *table, myfile_t *file);
+void free_queue(queue_t *queue);
+void free_hashtable(hashtable_t *hashtable);
+
+#endif //PROGETTOSOL_CACHE_H
