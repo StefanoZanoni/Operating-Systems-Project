87d17afb3caec3326d591864999f25838f79352a
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2022-01-04 11:23:44 +0100

    Update log.h

--- headers/server/log.h
+++ headers/server/log.h
@@ -1,7 +1,9 @@
 #ifndef PROGETTOSOL_LOG_H
 #define PROGETTOSOL_LOG_H
 
-FILE* start_log(char *path);
-void write_log(FILE* log, char *fmt, ...);
+FILE* start_log(const char *path);
+void write_log(FILE* log, const char *fmt, ...);
+
+extern FILE *my_log;
 
 #endif //PROGETTOSOL_LOG_H
