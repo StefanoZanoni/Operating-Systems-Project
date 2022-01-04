4e798a93c5b273596e49e1c734afd00e3047ae45
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2022-01-04 11:20:17 +0100

    Add files via upload

---
+++ headers/client/commands_execution.h
@@ -0,0 +1,9 @@
+#ifndef PROGETTOSOL_COMMANDS_EXECUTION_H
+#define PROGETTOSOL_COMMANDS_EXECUTION_H
+
+#include "./queue.h"
+
+void execute_h();
+int execute_command(commandline_arg_t command);
+
+#endif //PROGETTOSOL_COMMANDS_EXECUTION_H
