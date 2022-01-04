453dc9a43093d33b5b1898a6d886fd955d4dbd99
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2022-01-04 11:11:59 +0100

    Update configuration.c

--- src/server/configuration.c
+++ src/server/configuration.c
@@ -1,77 +1,72 @@
 #define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <errno.h>
 
-#include "../../includes/util.h"
-#include "../../includes/configuration.h"
-
-static FILE *config = NULL;
+#include "../../common/util.h"
+#include "../../headers/server/configuration.h"
 
 /*
     * this function is used to free the memory in case of error
     * or at the end of configuration file reading
 */
-void config_cleanup(cleanup_structure_t cstruct) {
+static void config_cleanup(char **vectors, FILE *config) {
 
-    int i = 3;
+    if (vectors) {
 
-    if (cstruct.pointers != NULL) {
+        if (vectors[3])
+            free(vectors[3]);
 
-        while (cstruct.pointers[i] != NULL) {
-
-            if ( ((char **) cstruct.pointers)[i] != NULL ) {
-                free( ((char **) cstruct.pointers)[i] );
-            }
-
-            i++;
-        }
+        if (vectors[4])
+            free(vectors[4]);
 
         //in case of error I have to free also sockname and logpath
         if (errno != 0) {
 
-            if ( ((char **) cstruct.pointers)[0] != NULL )
-                free( ((char **) cstruct.pointers)[0] );
-            if ( ((char **) cstruct.pointers)[1] != NULL )
-                free( ((char **) cstruct.pointers)[1] );
-            if ( ((char **) cstruct.pointers)[2] != NULL )
-                free( ((char **) cstruct.pointers)[2] );
+            if ( vectors[0] ) 
+                free( vectors[0] );
+
+            if ( vectors[1] ) 
+                free( vectors[1] );
+                
+            if ( vectors[2] ) 
+                free( vectors[2] );
+                
         }
+
     }
+
+    free(vectors);
 
     if (config)
         fclose(config);
-
-    free( (char **) cstruct.pointers );
-}
-
-void print_config(server_configuration_t configuration) {
-
-    if (configuration.sockname != NULL && configuration.logpath != NULL) {
-        printf("num_workers = %lu\n", configuration.num_workers);
-        printf("num_files = %lu\n", configuration.num_files);
-        printf("storage_capacity = %lu\n", configuration.storage_capacity);
-        printf("sockname = %s\n", configuration.sockname);
-        printf("logpath = %s\n", configuration.logpath);
-    }
 }
 
 /*
     * this function is used to read the configuration file and save the configuration parameters 
-    * into the server_cofiguration structure.
+    * into the server_configuration structure.
     * The configuration file is read line by line. 
 */
-void get_config(char *path, server_configuration_t *configuration) {
+void get_config(const char *path, server_configuration_t *configuration) {
+
+    if (!path || !configuration) {
+        errno = EINVAL;
+        fprintf(stderr, "Error reading server configuration");
+        exit(EXIT_FAILURE);
+    }
 
     //I prepare the facility for any cleanup in the event of a fatal error
-    cleanup_structure_t cstruct;
-    memset(&cstruct, 0, sizeof(cstruct));
-    cstruct.pointers = calloc(6, sizeof(char*));
-    CHECK_EQ_EXIT(config_cleanup(cstruct), "calloc config.c", cstruct.pointers, NULL, "cstruct.pointers\n", "");
+    char **vectors = NULL;
+    vectors = calloc(5, sizeof(char*));
+    CHECK_EQ_EXIT(config_cleanup(vectors, NULL), "calloc config.c", vectors, NULL, "vectors\n", "")
 
-    config = fopen(path, "r");
-    CHECK_EQ_EXIT(config_cleanup(cstruct), "fopen config.c", config, NULL, "config\n", "");
+    char *resolved_path = NULL;
+    resolved_path = realpath(path, NULL);
+    CHECK_EQ_EXIT(config_cleanup(vectors, NULL), "realpath config.c", resolved_path, NULL, "resolved_path\n", "")
+    FILE *config = fopen(resolved_path, "r");
+    free(resolved_path);
+    CHECK_EQ_EXIT(config_cleanup(vectors, config), "fopen config.c", config, NULL, "config\n", "")
 
     //line and n are used to store either each line of config file and its length
     char *line = NULL;
@@ -79,19 +74,18 @@ void get_config(char *path, server_configuration_t *configuration) {
 
     //informations is used to store the string before the actual server configuration data 
     char *informations = calloc(BUFSIZE, sizeof(char));
-    CHECK_EQ_EXIT(config_cleanup(cstruct), "calloc config.c", informations, NULL, "informations\n", "");
-    cstruct.pointers[3] = informations;
+    CHECK_EQ_EXIT(config_cleanup(vectors, config), "calloc config.c", informations, NULL, "informations\n", "")
+    vectors[3] = informations;
 
     //value is used to store acutal server configuration data
     char *value = calloc(BUFSIZE, sizeof(char));
-    CHECK_EQ_EXIT(config_cleanup(cstruct), "calloc config.c", value, NULL, "value\n", "");
-    cstruct.pointers[4] = value;
-    cstruct.pointers[5] = NULL;
+    CHECK_EQ_EXIT(config_cleanup(vectors, config), "calloc config.c", value, NULL, "value\n", "")
+    vectors[4] = value;
 
     while ( getline(&line, (size_t *) &n, config) != -1 ) {
 
         //in case of error on calloc "sockname" or "logpath", I need to free also line
-        cstruct.pointers[2] = line;
+        vectors[2] = line;
 
         //if the current line is empty, or it is a comment then continue
         if (line[0] == '#' || n <= 1)
@@ -99,36 +93,36 @@ void get_config(char *path, server_configuration_t *configuration) {
 
         sscanf(line, "%s = %s", informations, value);
 
-        if ( strstr(informations, "Threads" ) != NULL ) {
+        if ( strstr(informations, "Threads") != NULL ) {
 
             configuration->num_workers = strtoul(value, NULL, 10);
 
         }
-        else if ( strstr(informations, "Files" ) != NULL ) {
+        else if ( strstr(informations, "Files") != NULL ) {
 
             configuration->num_files = strtoul(value, NULL, 10);
 
         }
-        else if ( strstr(informations, "storage" ) != NULL ) {
+        else if ( strstr(informations, "storage") != NULL ) {
 
             configuration->storage_capacity = strtoul(value, NULL, 10);
 
         }
-        else if ( strstr(informations, "socket" ) != NULL ) {
+        else if ( strstr(informations, "socket") != NULL ) {
 
-            size_t len = strlen(value);
-            configuration->sockname = calloc(len + 1, sizeof(char));
-            cstruct.pointers[0] = configuration->sockname;
-            CHECK_EQ_EXIT(config_cleanup(cstruct), "calloc config.c", configuration->sockname, NULL, "sockname\n", "");
-            strncpy(configuration->sockname, value, len + 1);
+            size_t len = strlen(value) + 1;
+            configuration->sockname = calloc(len, sizeof(char));
+            vectors[0] = configuration->sockname;
+            CHECK_EQ_EXIT(config_cleanup(vectors, config), "calloc config.c", configuration->sockname, NULL, "sockname\n", "")
+            strcpy(configuration->sockname, value);
+
         }
         else if ( strstr(informations, "log" ) != NULL ) {
 
-            size_t len = strlen(value);
-            configuration->logpath = calloc(len + 1, sizeof(char));
-            cstruct.pointers[1] = configuration->logpath;
-            CHECK_EQ_EXIT(config_cleanup(cstruct), "calloc config.c", configuration->logpath, NULL, "logpath\n", "");
-            strncpy(configuration->logpath, value, len + 1);
+            configuration->logpath = realpath(value, configuration->logpath);
+            vectors[1] = configuration->logpath;
+            CHECK_EQ_EXIT(config_cleanup(vectors, config), "calloc config.c", configuration->logpath, NULL, "logpath\n", "")
+            
         }
 
         //to avoid memory leaks, line is freed at each iteration and at the end of all iterations
@@ -138,12 +132,12 @@ void get_config(char *path, server_configuration_t *configuration) {
         n = 0;
     }
 
-    cstruct.pointers[2] = line;
+    vectors[2] = line;
 
-    CHECK_EQ_EXIT(config_cleanup(cstruct), "getline config.c", errno, ENOMEM, "errno\n", "");
-    CHECK_EQ_EXIT(config_cleanup(cstruct), "getline config.c", errno, EINVAL, "errno\n", "");
+    CHECK_EQ_EXIT(config_cleanup(vectors, config), "getline config.c", errno, ENOMEM, "errno\n", "")
+    CHECK_EQ_EXIT(config_cleanup(vectors, config), "getline config.c", errno, EINVAL, "errno\n", "")
 
     free(line);
 
-    config_cleanup(cstruct);
+    config_cleanup(vectors, config);
 }
