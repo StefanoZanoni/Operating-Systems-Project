99bcbea4b5fc15c58d1ea4dc66f5c9fe2e55346f
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2022-01-11 12:48:48 +0100

    Update requests.h

--- headers/server/requests.h
+++ headers/server/requests.h
@@ -4,7 +4,27 @@
 #include "../../common/client_server.h"
 #include "./threadpool.h"
 
-int read_request(int client, server_command_t *request);
+/**
+ * This function is performed in parallel by each thread of the threadpool
+ *
+ * @param request client's request
+ * @param client the current client
+ * @param workers_pipe_wfd file descriptor of the pipe write entry
+ *
+ * @return -1 error (errno set)
+ * @return 0 success
+ */
 worker_func_t execute_request;
+
+/**
+ * This function is used to read a request from a client
+ *
+ * @param client the current client
+ * @param request structure on which the request will be stored
+ *
+ * @return -1 error (errno set)
+ * @return 0 success
+ */
+int read_request(int client, server_command_t *request);
 
 #endif //PROGETTOSOL_REQUESTS_H
