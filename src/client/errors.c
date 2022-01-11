50bc89da69ac7f27609fc5183041d0c602224831
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2022-01-11 16:37:08 +0100

    Update errors.c

--- src/client/errors.c
+++ src/client/errors.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
 #include <unistd.h>
 
-#include "../../headers/errors.h"
+#include "../../headers/client/errors.h"
 
 void my_perror(const char *string) {
 
@@ -11,70 +9,79 @@ void my_perror(const char *string) {
 
 		case ESOCK: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror("socket()");
+			fprintf(stderr, "\n\n");
 
 		} break;
 
 		case ECONN: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror("connect()");
+			fprintf(stderr, "\n\n");
 
 		} break;
 
 		case EREADN: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror("readn()");
+			fprintf(stderr, "\n\n");
 
 		} break;
 
 		case EWRITEN: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror("writen()");
+			fprintf(stderr, "\n\n");
 
 		} break;
 
 		case ESERVER: {
 
-			fprintf(stderr, "%s server error", string);
+			fprintf(stderr, "Error: %s server error\n\n", string);
 
 		} break;
 
 		case ECALLOC: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror("calloc()");
+			fprintf(stderr, "\n\n");
 
 		} break;
 
 		case ECLOSE: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror("close()");
+			fprintf(stderr, "\n\n");
 
 		} break;
 
 		case EREALLOC: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror("realloc()");
+			fprintf(stderr, "\n\n");
 
 		} break;
 
 		case EFOPEN: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror("fopen()");
+			fprintf(stderr, "\n\n");
 
 		} break;
 
 		default: {
 
-			fprintf(stderr, "%s ", string);
+			fprintf(stderr, "Error: %s ", string);
 			perror(NULL);
+			fprintf(stderr, "\n\n");
 
 		} break;
 	}
