6e01967d43bf25eff9054e15a01ef478958747f7
StefanoZanoni <71936224+StefanoZanoni@users.noreply.github.com>
2022-01-11 12:49:10 +0100

    Update signals.h

--- headers/server/signals.h
+++ headers/server/signals.h
@@ -9,7 +9,24 @@ typedef struct arg {
 
 } signal_handler_arg_t;
 
+/**
+ * This function is used to mask the signals: SIGINT, SIGQUIT, SIGHUP
+ *
+ * @param mask the new bits set
+ * @param oldmask the old bits set
+ *
+ * @return 0 success
+ * @return errno otherwise
+ */
 int block_signals(sigset_t *mask, sigset_t *oldmask);
+
+/**
+ * This function is performed by the thread handling the signals
+ *
+ * @param args file descriptor of the pipe write entry
+ *
+ * @return NULL
+ */
 void *signal_handler_thread(void *args);
 
 #endif //PROGETTOSOL_SIGNALS_H
